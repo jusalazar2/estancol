29cc5b65e1716c6afc095128beb5e9acf2f0852b
Juan Norrison Salazar Saenz <jsalasae@emeal.nttdata.com>
2024-06-09 08:58:41 -0500

    agregar git ignore

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\juan-\Videos\jhon\Estancol\venv
+set VIRTUAL_ENV=C:\Users\jsalasae\Videos\estancol\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
