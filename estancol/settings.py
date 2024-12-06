236cf993f0cb526e68a2ace4077937d19da847b3
jusalazar2 <128879903+jusalazar2@users.noreply.github.com>
2024-12-06 12:58:56 -0500

    Update settings.py

--- estancol/settings.py
+++ estancol/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Ruta donde se almacenarán los archivos estáticos recolectados
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
